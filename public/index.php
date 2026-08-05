85197ad77a98173557b882a15d38679f9f200d3c
Kingnike1 <161983544+Kingnike1@users.noreply.github.com>
2026-08-04 23:04:59 -0300

    feat(auth): load secure authentication routes

--- public/index.php
+++ public/index.php
@@ -4,21 +4,16 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 use App\Routes\Router;
 
-// Carregar variáveis de ambiente
 if (file_exists(__DIR__ . '/../.env')) {
     $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
     $dotenv->load();
 }
 
-// Configurações de erro
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-// Incluir as definições de rotas
 require_once __DIR__ . '/../routes/web.php';
+require_once __DIR__ . '/../routes/security.php';
 
-// Despachar a requisição
 Router::dispatch();
-
-?>
