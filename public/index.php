cbf471c3fe600feb0383ca97e424fa7d359aa3e9
Kingnike1 <161983544+Kingnike1@users.noreply.github.com>
2026-08-06 07:02:56 -0300

    refactor(router): dispatch route middleware centrally

--- public/index.php
+++ public/index.php
@@ -2,7 +2,6 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-use App\Middleware\RouteAuthorizationMiddleware;
 use App\Routes\Router;
 
 if (file_exists(__DIR__ . '/../.env')) {
@@ -15,7 +14,5 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 require_once __DIR__ . '/../routes/web.php';
-require_once __DIR__ . '/../routes/security.php';
 
-RouteAuthorizationMiddleware::handle((string) ($_SERVER['REQUEST_URI'] ?? '/'));
 Router::dispatch();
