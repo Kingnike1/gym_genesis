fb0ae537b06edf00ff20bee01908ca5f959864c8
Kingnike1 <161983544+Kingnike1@users.noreply.github.com>
2026-08-09 22:58:12 -0300

    feat(errors): register global error handling

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Container\Container;
+use App\Http\ErrorHandler;
 use App\Storage\LocalStorage;
 use App\Storage\StorageInterface;
 
@@ -13,9 +14,10 @@ if (file_exists(__DIR__ . '/../.env')) {
 $appEnv = (string) ($_ENV['APP_ENV'] ?? 'production');
 $appDebug = filter_var($_ENV['APP_DEBUG'] ?? false, FILTER_VALIDATE_BOOL);
 
-ini_set('display_errors', $appDebug ? '1' : '0');
-ini_set('display_startup_errors', $appDebug ? '1' : '0');
+ini_set('display_errors', '0');
+ini_set('display_startup_errors', '0');
 error_reporting(E_ALL);
+ErrorHandler::register($appDebug);
 
 date_default_timezone_set((string) ($_ENV['APP_TIMEZONE'] ?? 'America/Sao_Paulo'));
 
