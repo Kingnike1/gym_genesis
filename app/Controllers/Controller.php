ab8925fae2bd1a71be706ad9e14673fd8bfcda29
Kingnike1 <161983544+Kingnike1@users.noreply.github.com>
2026-08-11 22:25:31 -0300

    refactor(ui): use centralized URLs in base controller

--- app/Controllers/Controller.php
+++ app/Controllers/Controller.php
@@ -1,18 +1,22 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Controllers;
+
+use App\Routes\Router;
 
 class Controller
 {
     protected function render(string $viewPath, array $data = []): void
     {
         extract($data);
-        require_once __DIR__ . "/../Views/{$viewPath}.php";
+        require __DIR__ . "/../Views/{$viewPath}.php";
     }
 
     protected function redirect(string $path): void
     {
-        header("Location: /gym_genesis" . $path);
+        header('Location: ' . Router::url($path));
         exit();
     }
 }
